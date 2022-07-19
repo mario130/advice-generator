f9efb1852ef6e96c196851ef89b21dda463bc5ad
Mario Rizk <mario.rizk@softwaregroup.com>
2022-07-19 15:06:34 +0200

    add text animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import { Advice } from './features/counter/Advice';
-import axios from 'axios';
 
 function App() {
-	useEffect(() => {
-		axios.get('https://api.adviceslip.com/advice').then(res => {
-			console.log(`FROM APP ${res.data.slip.advice}`);
-		});
-	}, [])
-	
   return (
     <div className='App bg-secondary h-screen flex align-middle justify-center'>
 			<Advice />
--- src/features/counter/Advice.tsx
+++ src/features/counter/Advice.tsx
@@ -6,6 +6,7 @@ import {
 	selectAdvice,
 	selectId
 } from './adviceSlice';
+import AnimatedText from 'react-animated-text-content';
 
 export function Advice() {
 	const dispatch = useAppDispatch();
@@ -32,9 +33,28 @@ export function Advice() {
 	return (
 		<div className='m-auto bg-secondary-light rounded-xl p-6 max-w-sm text-white flex flex-col justify-center items-center transition-all relative'>
 			<p className='uppercase tracking-widest text-primary text-xs font-semibold'>Advice #{id}</p>
-			<p className='p-4 text-xl font-bold text-center leading-7'>
+			<AnimatedText
+				type="words" // animate words or chars
+				animation={{
+					x: '-50px',
+					y: '200px',
+					scale: 1.1,
+					ease: 'ease-in-out',
+				}}
+				animationType="blocks"
+				interval={0.04}
+				duration={0.3}
+				tag="p"
+				className="animated-paragraph p-4 text-xl font-bold text-center leading-7"
+				includeWhiteSpaces
+				threshold={0.1}
+				rootMargin="20%"
+			>
 				{advice}
-			</p>
+			</AnimatedText>
+			{/* <p className='p-4 text-xl font-bold text-center leading-7'>
+				{advice}
+			</p> */}
 			{/* <div className='bg-gray-600 h-px w-full mb-4'></div> */}
 			<img className='mb-6' src={`/images/pattern-divider-${isMobile ? 'mobile' : 'desktop'}.svg`} alt="" />
 			<button
